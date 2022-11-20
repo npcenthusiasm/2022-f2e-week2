c2997861b70c16985ed759884eed51f735d60b18
npcenthusiasm <npcenthusiasm@gmail.com>
2022-11-21 01:10:35 +0800

    feat: add deploy script

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: '2022 F2E - 第二關 今晚，我想來點點簽'
+    }
+  }
 })
